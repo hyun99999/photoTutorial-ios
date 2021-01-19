5947539f641896b46a409a114feeb67507d2475f
hyungyu Kim <ypsophila97@icloud.com>
2021-01-19 22:47:44 +0900

    Install pod to use image library, filter

--- photoTutorial-ios/ViewController.swift
+++ photoTutorial-ios/ViewController.swift
@@ -18,8 +18,15 @@ class ViewController: UIViewController {
         profileImage.layer.cornerRadius = profileImage.frame.height/2
         profileChangeBtn.layer.cornerRadius = 10
         
+//        profileChangeBtn.addTarget(self, action: #selector(onProfileChangeBtnClicked), for: .touchUpInside)
     }
 
-
+    @IBAction func onProfileChangeBtnClicked(_ sender: Any) {
+        
+    }
+    //    @objc fileprivate func onProfileChangeBtnClicked(){
+//        print("ViewController - onProfileChangeBtnClicked() called")
+//        
+//    }
 }
 
