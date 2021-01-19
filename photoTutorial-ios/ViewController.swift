afb05a892f855a2c1fe094cc509b0d7fa5d7500f
hyungyu Kim <ypsophila97@icloud.com>
2021-01-19 22:21:11 +0900

    Add profileImage,btn to change imageView

--- photoTutorial-ios/ViewController.swift
+++ photoTutorial-ios/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileChangeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileChangeBtn.layer.cornerRadius = 10
+        
     }
 
 
