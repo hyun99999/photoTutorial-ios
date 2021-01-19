ffe3f3ab342c6048c7af0df2a64aba78990fd771
hyungyu Kim <ypsophila97@icloud.com>
2021-01-19 23:45:18 +0900

    Use YPImagePicker for profileImage

--- photoTutorial-ios/ViewController.swift
+++ photoTutorial-ios/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class ViewController: UIViewController {
 
@@ -22,7 +23,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onProfileChangeBtnClicked(_ sender: Any) {
-        
+        print("ViewController - onProfileChangeBtnClicked() called")
+        let picker = YPImagePicker()
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
     }
     //    @objc fileprivate func onProfileChangeBtnClicked(){
 //        print("ViewController - onProfileChangeBtnClicked() called")
